ca3e4831c5c684067b0e86d5ad0425a83112762c
MateusGazzola <mateuseu232@gmail.com>
2025-06-11 21:23:52 -0300

    ultimoimput

--- test/component.test.js
+++ test/component.test.js
@@ -1,1 +1,47 @@
-function runComponentTests() {}
+function runComponentTests() {
+    testar("COMPONENT - Testar Se o Botão De Sortear esta Presente.", () => {
+        const sortear = document.getElementById("sortear");
+        if (!sortear) throw new Error("Botão Sortear Não Encontrado")
+    })
+    testar("COMPONENT - Inputs Máximo e Mínimo devem receber valores corretamente.", () => {
+        const min = document.getElementById("min");
+        const max = document.getElementById("max");
+
+        min.value = "10";
+        max.value = "5";
+
+        if (max.value !== "5" || min.value !== "10") {
+            throw new Error("Inputs não aceitaram valores corretamente");
+        }
+    });
+
+    testar("COMPONENT - Testar Se o Botão Repetir esta Presente.", () => {
+        const repetir = document.getElementById("repetir");
+        if (!repetir) throw new Error("Botão Repetir Não Encontrado");
+    });
+
+    testar("COMPONENT - Testar Se o Botão Copiar Resultado esta Presente.", () => {
+        const btnCopiar = document.getElementById("btnCopiar");
+        if (!btnCopiar) throw new Error("Botão Copiar Resultado Não Encontrado")
+    })
+
+    testar("COMPONENT - Input Quantidade deve receber valore corretamente.", () => {
+        const quantidade = document.getElementById("quantidade");
+        if (!quantidade) throw new Error("Inputs não aceitaram valores corretamente");
+    });
+
+    testar("COMPONENT - Testar Se a DIV resultado esta Presente.", () => {
+        const resultado = document.getElementById("resultado");
+        if (!resultado) throw new Error("DIV resultado Não Encontrado");
+    });
+
+    testar("COMPONENT - Testar Se Mensagem ERRO esta Presente.", () => {
+        const erro = document.getElementById("erro");
+        if (!erro) throw new Error("Mensagem ERRO Não Encontrado");
+    });
+
+        testar("COMPONENT - DIV Resultado Copiado esta Presente.", () => {
+        const copiadoMsg = document.getElementById("copiadoMsg");
+        if (!copiadoMsg) throw new Error("DIV Resultado Copiado Não Encontrado");
+    });
+}
--- test/unitario.test.js
+++ test/unitario.test.js
@@ -1,10 +1,37 @@
 function runUnitTests() {
-    testar("UNIT - Número Minimo Menor que Número Máximo", () => {
+    testar("UNIT - Minimo Menor que Número Máximo.", () => {
         let min = 10;
         let max = 20;
-    
-        if (min < max) {
+
+        if (min > max) {
             throw new Error(`Esperado que min (${min}) seja menor que max (${max})`);
 
-        }});
+        }
+    });
+    testar("UNIT - Número Quantidade Seja Pelo Menos 1.", () => {
+        if (quantidade < 1) {
+            throw new Error(`Esperado que a Quantidade Seja Pelo Menos 1.`);
+        }
+    })
+
+    testar("UNIT - O Número Quantidade não Pode Ser Maior que o Intervalo Disponivel.", () => {
+        const permitirRepetir = document.getElementById('repetir').checked;
+        const totalNumeros = max - min + 1;
+        if (!permitirRepetir && quantidade > totalNumeros) {
+            throw new Error(`Esperado que a Quantidade Não Seja Maior Que o Intervalo Disponivel.`);
+        }
+    })
+
+    testar("UNIT - Todos os Campos Devem Estar Preenchido Com Números Válido.", () => {
+        const minInput = document.getElementById('min').value;
+        const maxInput = document.getElementById('max').value;
+        const quantidadeInput = document.getElementById('quantidade').value;
+        const min = parseInt(10);
+        const max = parseInt(10);
+        const quantidade = parseInt(10);
+
+        if (isNaN(min) || isNaN(max) || isNaN(quantidade)) {
+            throw new Error(`Todos os Campo Devem Estar Preenchidos com Números Válidos.`);
+        }
+    })
 }
